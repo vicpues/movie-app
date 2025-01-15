df14de66105d0086d770c944deabdef4762720fd
MusabLodhi <musabdev65@gmail.com>
2025-01-15 14:34:15 +0500

    initial commit

---
+++ src/API/api.js
@@ -0,0 +1,8 @@
+const API_KEY = `3383bcd2696682b35f848f85a1f62690`;
+const moveID = 550;
+
+const getData = async () =>{
+    const response = await fetch(`https://api.themoviedb.org/3/movie/${moveID}?api_key=${API_KEY}`);
+    const data = await response.json();
+    console.log(data);
+};
