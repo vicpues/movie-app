d61f7566923308827cd2eb5e05b627fe7544e0e0
MusabLodhi <musabdev65@gmail.com>
2025-01-15 21:59:12 +0500

    add loader

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import Card from './Card';
-
+import loading from '../assets/loading.svg'
 // funcs
 import { fetchTrendingMovies } from '../API/api';
 const CardContainer = () => {
@@ -9,7 +9,6 @@ const CardContainer = () => {
     const fetchTrend = async () =>{
       const data = await fetchTrendingMovies();
       setTrending(data);
-      console.log(trending)
     };
     fetchTrend();
   }, [])
@@ -18,11 +17,11 @@ const CardContainer = () => {
   return (
     <div className='p-8'>
       <h1 className='font-semibold text-3xl text-zinc-600'>Discover</h1>
-      <div className='flex flex-wrap gap-4 mt-12'>
+      <div className='flex flex-wrap gap-4 mt-12 justify-center items-center h-[70vh]'>
         { trending.length > 0 ?
           trending.map((item, index) => <Card content={item} key={index}/>)
           :
-          null
+          <img src={loading} alt="loader" className='w-40' />
         }
       </div>
     </div>
