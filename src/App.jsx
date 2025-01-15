d2a7af9db5290935b3655a153b5775579cf5c60e
MusabLodhi <musabdev65@gmail.com>
2025-01-15 22:06:14 +0500

    fix ui bugs

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Main from "./components/Main";
 
 const App = () => {
   return (
-    <div className="flex">
+    <div className="flex h-screen">
       <SideBar />
       <Main />
     </div>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,7 +4,7 @@ import CardContainer from './CardContainer'
 
 const Main = () => {
   return (
-    <div className='flex-1'>
+    <div className='flex-1 overflow-y-auto'>
       <SearchBar />
       <CardContainer />
     </div>
