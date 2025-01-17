3730773d704ed9123ad54da3ed773979b5dd3a84
MusabLodhi <musabdev65@gmail.com>
2025-01-17 12:21:59 +0500

    add placeholder image for null posters

--- src/API/api.js
+++ src/API/api.js
@@ -3,6 +3,7 @@ const API_KEY = `3383bcd2696682b35f848f85a1f62690`;
 const getData = async (name) =>{
     const response = await fetch(`https://api.themoviedb.org/3/search/movie?query=${name}&language=en-US&page=1&api_key=${API_KEY}`);
     const data = await response.json();
+    console.log(data.results)
     return data.results
 };
 const fetchMovies = async (name) => {
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
-
+import placeholder from '../assets/placeholder.png'
 const Card = ({ content }) => {
-  const img = `https://image.tmdb.org/t/p/w500${content.poster_path}`;
+  let img;
+  if(content.poster_path === null){
+    img = placeholder;
+  }else{
+    img = `https://image.tmdb.org/t/p/w500${content.poster_path}`
+  }
   return (
     <div className='m-card w-[18%] hover:scale-105 transition duration-150 ease-out cursor-pointer' >
       <img src={img} alt="" className='w-full h-[400px] rounded-lg object-cover'/>
       <div>
-        <h3 className='mt-3 font-semibold text-lg'>{content.original_title}</h3>
+        <h3 className='mt-3 font-semibold text-lg leading-5'>{content.original_title}</h3>
         <div className='mt-2 flex flex-col'>
             <div className='flex gap-2'>
                 <h3 className='text-[#0929AD] text-sm'>Relase Date:</h3>
